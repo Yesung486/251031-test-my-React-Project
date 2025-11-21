50ea9062fb22fc96b8c73063fb2b20b8e398e995
yesung <yesung@iyeseong-ui-MacBookAir.local>
2025-11-21 13:23:39 +0900

    package.json homepage 주소 수정

--- App.tsx
+++ App.tsx
@@ -3,17 +3,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
-import SpaceShooter from './pages/SpaceShooter'; // 게임 페이지를 불러올게.
+import SpaceShooter from './pages/SpaceShooter';
 import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 주소가 그냥 `/` 일 때는 Homepage를 보여줘! */}
-        <Route path="/" element={<Homepage />} />
+  // 1. 우리 프로젝트의 '아파트 이름'을 여기에 정확하게 적어주는 거야.
+  const basename = "/251031-test-my-React-Project/";
 
-        {/* 주소가 `/neonbreaker` 일 때는 SpaceShooter 페이지(게임 페이지)를 보여줘! */}
+  return (
+    // 2. BrowserRouter에게 우리가 어디 사는지(basename) 알려주자!
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/neonbreaker" element={<SpaceShooter />} />
       </Routes>
     </BrowserRouter>
